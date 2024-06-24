b8a34671c4cff4ab5e578df21b32fe4e6f706107
daithehh04 <daithehh04@gmail.com>
2024-06-24 20:14:26 +0700

    fix api auth login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,47 @@
 const { SuccessResponse, CREATED } = require("../core/success.response.js")
-const AuthService = require("../services/auth.service.js")
+const bcrypt = require("bcrypt")
+const AuthService = require("../services/auth.service.js");
+const UserTransformer = require("../transformers/user.transformer.js");
 
 class AuthController {
   static login = async (req, res) => {
-    new SuccessResponse({
-      message: "Login Success!",
-      data: await AuthService.login(req.body),
-    }).send(res)
+    const { email, password } = req.body;
+    const obj = {
+      status: 200,
+      message: "Success!"
+    };
+    const findUser = await AuthService.findUser(email);
+    if (!findUser) {
+      obj.status = 403,
+      obj.message = "Not found user!"
+    } else {
+      const match = await bcrypt.compare(password, findUser.password)
+      if (!match) {
+        obj.status = 401,
+        obj.message = "UnAuthorization!"
+      } else {
+        const userTransformer = new UserTransformer(findUser)
+        obj.data = userTransformer;
+      }
+    }
+    res.status(obj.status).send(obj);
   }
   static signup = async (req, res) => {
-    new CREATED({
-      message: "Registered OK!",
-      data: await AuthService.signup(req.body),
-    }).send(res)
+    const { name, email, password } = req.body
+    const obj = {
+      status: 201,
+      message: "Success!"
+    };
+    const findUser = await AuthService.findUser(email);
+    if (findUser) {
+      obj.status = 403,
+      obj.message = "Account already registered!"
+    } else {
+      const passwordHash = await bcrypt.hash(password, 10)
+      const user = await AuthService.createUser({ name, email, password: passwordHash, status: 'active' })
+      obj.data = user
+    }
+    res.status(obj.status).send(obj);
   }
   static logout = async (req, res) => {
     new SuccessResponse({
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,39 +4,14 @@ const bcrypt = require("bcrypt")
 const { User } = require("../models/index")
 
 class AuthService {
-  static login = async ({ email, password }) => {
-    const findUser = await User.findOne({
+  static findUser = async (email) => {
+    return await User.findOne({
       where: { email },
     })
-    if (!findUser) {
-      return {
-        status:403,
-        message: "Not found user!"
-      }
-    }
-    const match = await bcrypt.compare(password, findUser.password)
-    if (!match) {
-      return {
-        status:401,
-        message: "UnAuthorization!"
-      }
-    }
-    const userTransformer = new UserTransformer(findUser)
-    return userTransformer;
   }
 
-  static signup = async ({ name, email, password }) => {
-    const user = await User.findOne({ where: { email } })
-    if (user) {
-      throw new BadRequestError("Error: Account already registered!")
-    }
-    const passwordHash = await bcrypt.hash(password, 10)
-    const newUser = await User.create({
-      name,
-      email,
-      password: passwordHash,
-      status: 'active'
-    })
+  static createUser = async (payload) => {
+    const newUser = await User.create(payload)
     const userTransformer = new UserTransformer(newUser)
     return userTransformer;
   }
